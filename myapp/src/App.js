4d01dcabf66d810d69d017bc868a333944adc2d8
Hiroto Murai <sundaycrafts@users.noreply.github.com>
2017-04-26 09:26:38 +0900

    1.4 React.DOM.*

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,19 @@ import './App.css';
 class App extends Component {
   render () {
     ReactDOM.render(
-      React.DOM.h1(null, 'Hello, world!'),
+      /** Result:
+       * <h1 id="my-heading">
+       *   <span><em>Hell</em>o</span>, world!
+       * </h1>
+      */
+      React.DOM.h1(
+        { id: 'my-heading' },
+        React.DOM.span(null,
+          React.DOM.em(null, 'Hell'),
+          'o'
+        ),
+        ', world!'
+      ),
       document.getElementById('root')
     )
   }
