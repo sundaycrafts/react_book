37fa5772e51c7c1d58d07d2e63c78222d77311c4
Hiroto Murai <sundaycrafts@users.noreply.github.com>
2017-05-06 22:00:17 +0900

    solve component error occured by ES6 syntax

--- myapp/src/text-area-counter.js
+++ myapp/src/text-area-counter.js
@@ -1,31 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logMixin from './log-mixin'
 
-export default React.createClass({
-  name: 'TextAreaCounter',
-  mixins: [logMixin],
-  propTypes: {
-    text: React.PropTypes.string
-  },
-  getDefaultProps: function () {
-    return {
-      defaultValue: ''
+export default class TextAreaCounter extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: this.props.defaultValue,
+      name: 'TextAreaCounter'
     }
-  },
-  getInitialState: function () {
-    return { text: this.props.defaultValue }
-  },
-  _textChange: function (ev) {
-    this.setState({
-      text: ev.target.value
-    })
-  },
-  componentDidUpdate: function (oldProps, oldState) {
-    if (this.state.text.length > 3) {
-      this.replaceState(oldState)
-    }
-  },
-  render: function () {
+  }
+
+  mixins: [logMixin]
+
+  static defaultProps: {
+    defaultValue: ''
+  }
+
+  render () {
     return React.DOM.div(null,
       React.DOM.textarea({
         value: this.state.text,
@@ -34,4 +25,34 @@ export default React.createClass({
       React.DOM.h3(null, this.state.text.length)
     )
   }
-})
+
+  propTypes: {
+    text: React.PropTypes.string
+  }
+
+  /**
+   * event handlers should bind to "this".
+   * alternatively, override it by binded function
+   * in constructor like bellow(verbose):
+   * this._textChange = this._textChange.bind(this)
+   */
+  _textChange = ev => {
+    this.setState({
+      text: ev.target.value
+    })
+  }
+
+  /**
+   * Life-cycle event(s)
+   */
+  componentDidUpdate (oldProps, oldState) {
+    if (this.state.text.length > 3) {
+      /**
+       * attention: replaceState is duplicated
+       * https://github.com/facebook/react/issues/3236
+       */
+      // this.replaceState(oldState)
+      this.setState(oldState)
+    }
+  }
+}
