c3d0b303ff3121890651eac38065bed824f5e489
Hiroto Murai <sundaycrafts@users.noreply.github.com>
2017-05-30 22:25:15 +0900

    6.2.9 FormInput factory component

--- whinepad/src/App.js
+++ whinepad/src/App.js
@@ -5,6 +5,7 @@ import Logo from './components/Logo'
 import Button from './components/Button'
 import Suggest from './components/Suggest'
 import Rating from './components/Rating'
+import FormInput from './components/FormInput'
 
 class App extends Component {
   render () {
@@ -29,6 +30,38 @@ class App extends Component {
         <div>Init value 4: <Rating defaultValue={4} /></div>
         <div>Max value 11: <Rating max={11} /></div>
         <div>Read only: <Rating readonly={true} defaultValue={3} /></div>
+
+        <h2>FormInput</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>単純な入力フィールド</td>
+              <td><FormInput /></td>
+            </tr>
+            <tr>
+              <td>デフォルト値</td>
+              <td><FormInput defaultValue='デフォルトです' /></td>
+            </tr>
+            <tr>
+              <td>年の入力</td>
+              <td><FormInput type='year' /></td>
+            </tr>
+            <tr>
+              <td>評価</td>
+              <td><FormInput type='rating' defaultValue={4} /></td>
+            </tr>
+            <tr>
+              <td>入力候補の提示</td>
+              <td><FormInput type='suggest'
+                options={['red', 'green', 'blue']}
+                defaultValue='green' /></td>
+            </tr>
+            <tr>
+              <td>単純なテキストエリア</td>
+              <td><FormInput type='text' /></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
---
+++ whinepad/src/components/FormInput.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Suggest from './Suggest'
+import Rating from './Rating'
+
+class FormInput extends Component {
+  render () {
+    const common = {
+      id: this.props.id,
+      ref: 'input',
+      defaultValue: this.props.defaultValue
+    }
+
+    switch (this.props.type) {
+      case 'year':
+        return (
+          <input
+            {...common}
+            type='number'
+            defaultValue={this.props.defaultValue || new Date().getFullYear()}
+          />
+        )
+      case 'suggest':
+        return <Suggest {...common} options={this.props.options} />
+      case 'rating':
+        return (
+          <Rating
+            {...common}
+            defaultValue={parseInt(this.props.defaultValue, 10)}
+          />
+        )
+      case 'text':
+        return <textarea {...common} />
+      default:
+        return <input {...common} type='text' />
+    }
+  }
+}
+
+export default FormInput
